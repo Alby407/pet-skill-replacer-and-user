afde439d83e799d5f54d62c6bb365799340b6b9f
Albert Schotschneider <schotschneider@gmail.com>
2021-01-06 15:31:30 +0100

    Update for new patch.

--- index.js
+++ index.js
@@ -1,57 +1,50 @@
 module.exports = function pet_skills(mod) {
-	
-	const command = mod.command;
-	let config = require('./config.json');
-	let petId = 0n;
-	let enabled = true;
-	
-	mod.game.initialize("me");
-	
-	command.add('petr', {
-		$none() {
-			enabled = !enabled;
-			command.message(`${enabled ? 'En' : 'Dis'}abled`);
-		}
-	});
-	
-    mod.hook('C_START_SERVANT_ACTIVE_SKILL', 1, event => {
-		petId = event.gameId;
-		if(enabled && config.ReplaceAbility && ((event.skill >= 1101 && event.skill < 1138) || event.skill === 1001))
-		{
-			event.skill = config.NewPartnerAbility;
-			return true;
-		}
+
+    const command = mod.command;
+    let config = require('./config.json');
+    let petId = 0n;
+    let enabled = true;
+
+    mod.game.initialize("me");
+
+    command.add('petr', {
+        $none() {
+            enabled = !enabled;
+            command.message(`${enabled ? 'En' : 'Dis'}abled`);
+        }
     });
-	
-	mod.hook('C_USE_ITEM', 3, event => {
-		if (enabled && config.ReplaceItems && config.TheItems[event.id])
-		{
-			if(petId)
-			{
-				mod.send('C_START_SERVANT_ACTIVE_SKILL', 1, {
-					gameId: petId,
-					skill: config.TheItems[event.id]
-				});
-			}
-			else
-			{
-				command.message("Partner not found - You need to summon him to use his partner skill");
-			}
-			return false;
-		}
-	});
-	
-	mod.hook('S_REQUEST_SPAWN_SERVANT', 1, event => {
-		if(event.ownerId === mod.game.me.gameId)
-		{
-			petId = event.gameId;
-		}
-	});
-	
-	mod.hook('S_REQUEST_DESPAWN_SERVANT', 1, event => {
-		if(event.gameId === petId)
-		{
-			petId = 0n;
-		}
-	});
+
+    mod.hook('C_START_SERVANT_ACTIVE_SKILL', 1, event => {
+        petId = event.gameId;
+        if (enabled && config.ReplaceAbility && ((event.skill >= 1101 && event.skill < 1138) || event.skill === 1001)) {
+            event.skill = config.NewPartnerAbility;
+            return true;
+        }
+    });
+
+    mod.hook('C_USE_ITEM', 3, event => {
+        if (enabled && config.ReplaceItems && config.TheItems[event.id]) {
+            if (petId) {
+                mod.send('C_START_SERVANT_ACTIVE_SKILL', 1, {
+                    gameId: petId,
+                    skill: config.TheItems[event.id]
+                });
+            } else {
+                command.message("Partner not found - You need to summon him to use his partner skill");
+            }
+            return false;
+        }
+    });
+
+    mod.hook('S_REQUEST_SPAWN_SERVANT', 4, event => {
+        if (event.ownerId === mod.game.me.gameId) {
+            petId = event.gameId;
+        }
+    });
+
+    mod.hook('S_REQUEST_DESPAWN_SERVANT', 1, event => {
+        if (event.gameId === petId) {
+            petId = 0n;
+        }
+    });
 }
